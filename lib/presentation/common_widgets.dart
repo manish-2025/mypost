79ed5622522d000a31d5faad0c2c488913859f4f
MeManishPatel <me.manishpatel2025@gmail.com>
2025-04-19 22:16:25 +0530

    Update common_widgets.dart

--- lib/presentation/common_widgets.dart
+++ lib/presentation/common_widgets.dart
@@ -1,10 +1,6 @@
 // ignore_for_file: use_build_context_synchronously
 
-import 'dart:io';
-
-import 'package:cross_file/src/types/interface.dart';
 import 'package:flutter/material.dart';
-import 'package:image_cropper/image_cropper.dart';
 import 'package:mypost/common/app_colors.dart';
 import 'package:mypost/data/entity/quote_entity/quote_entity.dart';
 import 'package:mypost/globals.dart';
